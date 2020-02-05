7695b82709eba20f85b320445c915e04007990a3
jordancov <60715643+jordancov@users.noreply.github.com>
2020-02-05 21:58:52 +0000

    Add - index.php, style.css

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Jordan Coverdale</title>
+  <meta name="author" content="name">
+  <meta name="description" content="description here">
+  <meta name="keywords" content="keywords,here">
+  <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  </head>
+  <body>
+    <div class="welcome-section">
+        <h1>Welcome!</h1>
+    </div>
+  </body>
+</html>
